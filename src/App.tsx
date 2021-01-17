c72a9c03c732b36f6f36547f20a2a16b175b46b8
Tim Rybicki <t.rybicki@gmail.com>
2021-01-17 15:12:59 -0500

    Setup babel path aliases

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { NativeRouter, Route, Switch } from 'react-router-native';
 import { ThemeProvider } from 'react-native-elements';
-import HomePage from './components/HomePage/HomePage';
-import CreateSessionPage from './components/CreateSessionPage/CreateSessionPage';
+import HomePage from 'src/components/HomePage/HomePage';
+import CreateSessionPage from 'src/components/CreateSessionPage/CreateSessionPage';
 
 export default function App() {
   return (
