332eaf88d02edd031186be94a8eb4aff927e5c70
Tim Rybicki <t.rybicki@gmail.com>
2021-01-17 14:44:09 -0500

    Create Route Switch and separate components

--- App.tsx
+++ App.tsx
@@ -1,19 +1,50 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { NativeRouter, Route, Link, useHistory, Switch } from 'react-router-native';
 import { Button, ThemeProvider, Text } from 'react-native-elements';
 import styled from 'styled-components/native';
 
-export default function App() {
+const CreateSessionPage = () => {
   return (
-    <ThemeProvider>
-      <StatusBar hidden />
+    <Container>
+      <Text>Create Page</Text>
+    </Container>
+  );
+};
+
+const HomePage = () => {
+  const history = useHistory();
+
+  return (
+    <>
       <Container>
-        <Button buttonStyle={{ marginBottom: 50 }} onPress={() => true} title="Create New" />
+        <Link to="/create">
+          <Button
+            buttonStyle={{ marginBottom: 50 }}
+            onPress={() => history.push('/create')}
+            title="Create New Session"
+          />
+        </Link>
         <Button buttonStyle={{ marginBottom: 50 }} onPress={() => true} title="Load Previous" />
       </Container>
       <Container>
         <Text h4>Past sessions will appear here</Text>
       </Container>
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <NativeRouter>
+        <StatusBar hidden />
+
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/create" component={CreateSessionPage} />
+        </Switch>
+      </NativeRouter>
     </ThemeProvider>
   );
 }
