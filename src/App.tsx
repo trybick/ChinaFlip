ffe755f9fa470bdc31df4890af7329109af6440c
Tim Rybicki <t.rybicki@gmail.com>
2021-08-15 09:34:40 -0400

    Add routing constants to app component

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { BackButton, NativeRouter, Route, Switch } from 'react-router-native';
 import { ThemeProvider } from 'react-native-elements';
+import { ROUTES } from 'utils/routes';
 import HomePage from 'src/components/HomePage/HomePage';
 import WordListPage from 'src/components/WordListPage/WordListPage';
 
@@ -14,8 +15,8 @@ export default function App() {
         <StatusBar style="auto" />
 
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/wordlist/:id" component={WordListPage} />
+          <Route exact path={ROUTES.HOME} component={HomePage} />
+          <Route exact path={`${ROUTES.WORD_LIST}/:id`} component={WordListPage} />
         </Switch>
       </NativeRouter>
     </ThemeProvider>
