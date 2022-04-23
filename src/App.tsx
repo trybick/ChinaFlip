5545fef61483a139c72cedc028bcab94daf29c40
trybick <t.rybicki@gmail.com>
2022-04-23 18:59:56 -0400

    Fix transportation not playing

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import WordListPage from 'components/WordListPage/WordListPage';
 
 Sentry.init({
   dsn: 'https://630644df65d24a64a14aa5e819cbf9fe@o367043.ingest.sentry.io/6357720',
-  enableInExpoDevelopment: true,
+  enableInExpoDevelopment: false,
   debug: __DEV__,
 });
 
