663f564994512458183d33c1fd16758ac708ad20
trybick <t.rybicki@gmail.com>
2022-04-23 08:18:08 -0400

    Add sentry packages and init

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { BackButton, NativeRouter, Route, Switch } from 'react-router-native';
 import { ThemeProvider } from 'react-native-elements';
+import * as Sentry from 'sentry-expo';
 import { ROUTES } from 'utils/routes';
 import { loadSounds } from 'utils/soundPlayer';
 import HomePage from 'components/HomePage/HomePage';
 import WordListPage from 'components/WordListPage/WordListPage';
+
+Sentry.init({
+  dsn: 'https://630644df65d24a64a14aa5e819cbf9fe@o367043.ingest.sentry.io/6357720',
+  enableInExpoDevelopment: true,
+  debug: __DEV__,
+});
 
 export default function App() {
   useEffect(() => {
